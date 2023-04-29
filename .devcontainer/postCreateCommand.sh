fc3613da5dbe89b950001017ff37975b83d275bf
hexsorcerer <hexsorcerer03e9@gmail.com>
2023-04-29 21:10:42 +0000

    chore: add git config in devcontainer
    Added the git global configuration and a .env file to set the values.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,5 +2,5 @@
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sed -i 's/ZSH_THEME=.*/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/' ~/.zshrc
-sed -i 's/plugins=.*/plugins=\(git ssh-agent\)/' ~/.zshrc
-
+git config --global user.email "$1"
+git config --global user.name "$2"
