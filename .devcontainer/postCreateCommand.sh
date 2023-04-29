318647d292d3df8b12931d105f3c30973cb0f5e2
hexsorcerer <hexsorcerer03e9@gmail.com>
2023-04-29 21:30:23 +0000

    chore: add p10k zsh prompt config
    Also need this file for the prompt config to work.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-sed -i 's/ZSH_THEME=.*/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/' ~/.zshrc
+cp .devcontainer/.zshrc ~/.zshrc
+cp .devcontainer/.p10k.zsh ~/.p10k.zsh
 git config --global user.email "$1"
 git config --global user.name "$2"
